6a8eaf1cc9e76fef66d416f47be2ba71a5f7daa7
11Geoffreyg <geoffreyjguez@gmail.com>
2020-04-20 11:41:44 +0200

    first props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  const helloworld = 'Welcome to the Road to React';
+function App(props) {
+  const helloworld = 'Bonjour';
   return (
     <div className="App">
-      <h2>{helloworld}</h2>
+      <h2>{helloworld} {props.user}</h2>
     </div>
   );
 }
