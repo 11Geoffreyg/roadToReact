1d0389cf6b80b3e48b8e07acc404a78a36f2a700
11Geoffreyg <geoffreyjguez@gmail.com>
2020-04-22 09:46:27 +0200

    add HMR

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
     <App user="Geoffrey Guez" />
-  </React.StrictMode>,
-  document.getElementById('root')
+  ,document.getElementById('root')
 );
+
+if(module.hot){
+  module.hot.accept();
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
