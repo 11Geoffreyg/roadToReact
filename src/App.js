40cd666316d681ca864e5074f2aaa0e66d54992d
11Geoffreyg <geoffreyjguez@gmail.com>
2020-04-22 10:50:31 +0200

    ES6 arrow functions

--- src/App.js
+++ src/App.js
@@ -27,8 +27,7 @@ function App(props) {
   return (
     <div className="App">
       <h2>{helloworld} {props.user}</h2>
-      {list.map(function(item){
-        return (
+      {list.map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -37,8 +36,7 @@ function App(props) {
             <span>{item.num_comments} </span>
             <span>{item.points} </span>
           </div>
-        );
-      })}
+      )}
     </div>
   );
 }
