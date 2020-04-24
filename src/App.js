401bf462696157a0186b8770cd1f13d9e0da78ce
11Geoffreyg <geoffreyjguez@gmail.com>
2020-04-24 14:39:36 +0200

    search bar

--- src/App.js
+++ src/App.js
@@ -19,24 +19,66 @@ const helloworld = 'Bonjour';
       points: 5,
       objectID: 1,
     },
+    {
+      title: 'Google',
+      url: 'https://google.com/',
+      author: 'No one',
+      num_comments: 6,
+      points: 204,
+      objectID: 2,
+    },
+    {
+      title: 'Facebook',
+      url: 'https://facebook.com/',
+      author: 'Mark Zukerberg',
+      num_comments: 1,
+      points: 8,
+      objectID: 3,
+    },
   ];
   const name = 'name'
-
+const isSearched = searchTerm => item =>
+    item.title.toLowerCase().includes(searchTerm);
 class App extends Component {
   constructor(props) {
       super(props);
       this.state = {
         list,
         [name]: "Geoffrey Guez",
-        count1: 3,
-        count2: 5,
+        count: 0,
+        count2: 0,
+        serachTerm: '',
       };
+      this.onDismiss = this.onDismiss.bind(this);
+      this.onClickme = this.onClickme.bind(this);
+      this.onSearchChange = this.onSearchChange.bind(this);
+  }
+  onDismiss(id){
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({list: updatedList});
+  }
+  onClickme(){
+    const countUpdate = this.state.count+1;
+    this.setState( {count: countUpdate});
+  }
+  onSearchChange(event){
+    this.setState({searchTerm: event.target.value});
   }
   render() {
     return (
       <div className="App">
         <h2>{helloworld+' '+this.state.name}</h2>
-        {this.state.list.map(item =>
+        <button onClick={this.onClickme} type='button'>Click me !</button>
+        <h2>{this.state.count}</h2>
+        <form>
+          <input 
+          type='text'
+          onChange={this.onSearchChange}
+          >
+
+          </input>
+        </form>
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
@@ -44,6 +86,14 @@ class App extends Component {
               <span> {item.author} </span>
               <span>{item.num_comments} </span>
               <span>{item.points} </span>
+              <span>
+                <button 
+                onClick={() => this.onDismiss(item.objectID)}
+                type='button'
+                >
+                  Supprimer
+                </button>
+              </span>
             </div>
         )}
       </div>
